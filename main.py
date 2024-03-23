d574d32eae153f22e80155405995f5f8b9597121
Misha <mmelihow@gmail.com>
2024-03-23 17:16:35 +0300

    заполнен main.py - следующий шаг хендлеры

---
+++ main.py
@@ -0,0 +1,20 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+
+from config_data.config import Config, load_config
+
+# Функция конфигурирования и запуска бота
+async def main() -> None:
+    
+    config: Config = load_config()
+    
+    bot = Bot(token=config.tg_bot.token)
+    dp = Dispatcher()
+   
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+asyncio.run(main())
+    
