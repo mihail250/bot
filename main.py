72df0dc729ca2aadc8d5625aebfe6fe940f9c402
Misha <mmelihow@gmail.com>
2024-03-24 16:40:00 +0300

    finished ROUTERS

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 from aiogram import Bot, Dispatcher
+from handlers import user_handlers, other_handlers
 
 from config_data.config import Config, load_config
 
@@ -11,6 +12,8 @@ async def main() -> None:
     bot = Bot(token=config.tg_bot.token)
     dp = Dispatcher()
    
+    dp.include_router(user_handlers.router)
+    dp.include_router(other_handlers.router)
 
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
